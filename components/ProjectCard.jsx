70e8f3cd06100987de51b5047d059fe0970a0710
Harshit Patel <hsp2810@gmail.com>
2024-04-11 10:01:38 -0600

    Done with the bugs

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const ProjectCard = ({ project }) => {
+  const videoRef = useRef(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleVideoLoad = () => {
+    setIsLoaded(true);
+  };
+
+  // if (!isLoaded) return <div>Loading...</div>;
+
   return (
     <>
       {project && (
@@ -9,21 +18,29 @@ const ProjectCard = ({ project }) => {
           <div className='md:hidden rounded-2xl mt-6'>
             {project.photo ? (
               <img
-                className='rounded-lg h-1/2 w-full'
+                className='rounded-2xl h-1/2 w-full'
                 src={project.photo}
                 alt='Project card'
                 height={100}
                 width={100}
               />
             ) : (
-              <video
-                src={project.video}
-                className='rounded-lg w-full opacity-65'
-                height={100}
-                width={100}
-                autoPlay
-                controls={false}
-              />
+              <div className='relative w-full'>
+                <video
+                  ref={videoRef}
+                  src={project.video}
+                  className='rounded-2xl w-full opacity-65'
+                  height={100}
+                  width={100}
+                  controls={true}
+                />
+                <div className='absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center'>
+                  <div className='bg-black bg-opacity-25 p-2 rounded-r-lg'>
+                    <PlayBtn videoRef={videoRef} />
+                    <PauseBtn videoRef={videoRef} />
+                  </div>
+                </div>
+              </div>
             )}
             <div className='flex flex-col items-start py-4'>
               <div className='font-bold text-2xl mt-2 mb-2 text-primaryColor'>
@@ -37,24 +54,28 @@ const ProjectCard = ({ project }) => {
 
           {/* Screens */}
           <div className='hidden md:block relative rounded-2xl mt-2 md:ml-3 overflow-hidden h-full'>
-            {project.photo ? (
-              <img
-                className='rounded-lg w-full opacity-65'
-                src={project.photo}
-                alt='Project card'
-                height={100}
-                width={100}
-              />
-            ) : (
-              <video
-                src={project.video}
-                className='rounded-lg w-full opacity-65'
-                height={100}
-                width={100}
-                autoPlay
-                controls={false}
-              />
-            )}
+            <div className='relative w-full'>
+              {project.photo ? (
+                <img
+                  className='rounded-2xl w-full opacity-65'
+                  src={project.photo}
+                  alt='Project card'
+                  height={100}
+                  width={100}
+                />
+              ) : (
+                <video
+                  ref={videoRef}
+                  src={project.video}
+                  className='rounded-2xl w-full opacity-65'
+                  height={100}
+                  width={100}
+                  controls={false}
+                  onLoadedData={handleVideoLoad}
+                  style={{ display: isLoaded ? "block" : "none" }}
+                />
+              )}
+            </div>
             <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-90 transition-opacity opacity-0 hover:opacity-100'>
               <a
                 href={project.githubUrl}
@@ -65,16 +86,24 @@ const ProjectCard = ({ project }) => {
               </a>
               <div className='absolute bottom-0 left-0 text-white p-5 transition-opacity duration-300 hover:opacity-100'>
                 <h1 className='flex items-center font-extrabold text-5xl text-left bg-gradient-to-r from-primaryColor to-blue-900 text-transparent bg-clip-text bg-no-repeat bg-cover w-fit'>
-                  {project.title}{" "}
-                  <span className='bg-white text-xs font-normal ml-1 text-black p-1 rounded-lg hover:bg-gray-200'>
-                    {project.isGoing && `ONGOING`}
-                  </span>
+                  {project.title}
+                  {project.isGoing && (
+                    <span className='bg-white text-xs font-normal ml-1 text-black p-1 rounded-lg hover:bg-gray-200'>
+                      ONGOING
+                    </span>
+                  )}
                 </h1>
                 <p className='text-md font-light text-left'>
                   {project.description}
                 </p>
               </div>
             </div>
+            {project.video && (
+              <div className='absolute bottom-0 w-full p-2 rounded-r-lg'>
+                <PlayBtn videoRef={videoRef} />
+                <PauseBtn videoRef={videoRef} />
+              </div>
+            )}
           </div>
         </>
       )}
@@ -83,3 +112,52 @@ const ProjectCard = ({ project }) => {
 };
 
 export default ProjectCard;
+
+const PlayBtn = ({ videoRef }) => {
+  return (
+    <button
+      onClick={() => videoRef.current.play()}
+      className='rounded-full hover:bg-primaryColor hover:text-white p-2 transition'
+    >
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        className='lucide lucide-play'
+      >
+        <polygon points='6 3 20 12 6 21 6 3' />
+      </svg>
+    </button>
+  );
+};
+
+const PauseBtn = ({ videoRef }) => {
+  return (
+    <button
+      onClick={() => videoRef.current.pause()}
+      className='rounded-full hover:bg-primaryColor hover:text-white p-2 transition'
+    >
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        className='lucide lucide-pause'
+      >
+        <rect x='14' y='4' width='4' height='16' rx='1' />
+        <rect x='6' y='4' width='4' height='16' rx='1' />
+      </svg>
+    </button>
+  );
+};
--- utils/data/projects.js
+++ utils/data/projects.js
@@ -2,21 +2,21 @@ export const projects = [
   {
     title: "Social Media Application",
     description:
-      "NewsFreak is a news fetching application that uses the newsapi to fetch and display top headlines for users. With various functionalities such as sorting, changing the news category and language, NewsFreak provides a personalized experience for each user. Developed as a course project during the final semester, NewsFreak showcases the ability to integrate APIs and create functional applications.",
+      "Introducing a dynamic social media platform where connections thrive! Our app empowers users to engage with others through seamless follow mechanisms, fostering both public and private interactions. Share your moments through posts and stories, while building meaningful connections through comments and interactions with like-minded individuals.",
 
     githubUrl: "https://github.com/hsp2810/Sastagram",
     video:
-      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712775672/sastagram_fvwjw7.mp4",
+      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712848708/portfolio/sastagram_smfqjb.mp4",
     isGoing: true,
   },
   {
     title: "YumRush",
     description:
-      "NewsFreak is a news fetching application that uses the newsapi to fetch and display top headlines for users. With various functionalities such as sorting, changing the news category and language, NewsFreak provides a personalized experience for each user. Developed as a course project during the final semester, NewsFreak showcases the ability to integrate APIs and create functional applications.",
+      "Introducing YumRush, your go-to solution for satisfying cravings in a snap! With an intuitive interface, users can effortlessly explore a wide array of cuisines, quickly locate their desired dishes, and place orders seamlessly within the app. Whether it's a spontaneous craving or a planned culinary adventure, YumRush delivers convenience and variety at your fingertips.",
 
     githubUrl: "https://github.com/hsp2810/noteslele",
     video:
-      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712773547/yumrush_lwshrc.mp4",
+      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712848703/portfolio/yumrush_fjmh3v.mp4",
     isGoing: true,
   },
   {
@@ -36,7 +36,7 @@ export const projects = [
 
     githubUrl: "https://github.com/hsp2810/PoeticGuru",
     video:
-      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712728131/portfolio/poeticguru_o0yaaf.mp4",
+      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712848691/portfolio/poeticguru_ggcps8.mp4",
     isGoing: false,
   },
   {
@@ -46,17 +46,17 @@ export const projects = [
 
     githubUrl: "https://github.com/hsp2810/BuyGoodSellGood",
     video:
-      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712769028/portfolio/buygoodsellgood_t6cxgr.mp4",
+      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712848688/portfolio/buygoodsellgood_pczthq.mp4",
     isGoing: false,
   },
   {
     title: "HomeNventory",
     description:
-      "BuyAndSell is a marketplace application designed for the SAIT community where staff can buy and sell items from each other. With a simple and secure platform, BuyAndSell makes it easy for staff to connect and buy/sell items within their own community, fostering a sense of community and support.",
+      "Introducing HomeNventory, your essential home organization companion! This user-friendly CRUD (Create, Read, Update, Delete) app streamlines household management with secure user authentication. Easily create rooms and effortlessly add, edit, or remove items as needed. Simplify your home organization journey with HomeNventory's intuitive functionality, putting control at your fingertips.",
 
     githubUrl: "https://github.com/hsp2810/BuyGoodSellGood",
     video:
-      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712771042/portfolio/homenventory_jdvkje.mp4",
+      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712848708/portfolio/homenventory_xgrizc.mp4",
     isGoing: false,
   },
   {
@@ -66,18 +66,18 @@ export const projects = [
 
     githubUrl: "https://github.com/hsp2810/NewsFreak",
     video:
-      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712771449/portfolio/newsfreak_truhvs.mp4",
+      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712848692/portfolio/newsfreak_yken8w.mp4",
     isGoing: false,
   },
 
   {
     title: "Noteslele",
     description:
-      "NewsFreak is a news fetching application that uses the newsapi to fetch and display top headlines for users. With various functionalities such as sorting, changing the news category and language, NewsFreak provides a personalized experience for each user. Developed as a course project during the final semester, NewsFreak showcases the ability to integrate APIs and create functional applications.",
+      "Meet Noteslele, your funky yet functional task management solution! Sporting a vibrant design crafted with Tailwind CSS, this simple CRUD app offers a hassle-free way to store and manage your important tasks. Easily add, edit, and delete tasks as needed, empowering you to stay organized and focused on what matters most. Say goodbye to clutter and hello to productivity with Noteslele!",
 
     githubUrl: "https://github.com/hsp2810/noteslele",
     video:
-      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712772642/portfolio/noteslele_fkapt8.mp4",
+      "https://res.cloudinary.com/dipe21kuj/video/upload/v1712848687/portfolio/noteslele_maczya.mp4",
     isGoing: false,
   },
 ];
